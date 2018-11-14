03c8ce463a211bf5776e8a3b17e9ca295bc4ce86
thepushkarp <pushkar.patel.21.1.2001p@gmail.com>
2018-11-14 20:10:00 +0530

    Refine code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<unistd.h>
 #include<fstream>
 #include"Dep.h"
 #include"Recomm.h"
@@ -10,19 +11,28 @@ using namespace std;
 
 int main()
 {
-    cout<<TXT_B_RED<<"DEPRESSION "<<TXT_B_GREEN<<"TESTER"<<RESET<<"\n\n";
+    system("clear");
+    cout<<TXT_B_RED<<"DEPRESSION TESTER"<<RESET<<"\n\n";
     int choice;
+    cout<<TXT_B_WHITE;
     cout<<"Enter any one option from the following:\n\n";
-    cout<<"1. Run Depression Tester\n";
-    cout<<"2. View Records\n";
-    cout<<"3. Delete Record\n";
-    cout<<"4. View About Section and Disclaimer\n";
-    cout<<"\n To exit program, enter 0.\n";
+    cout<<TXT_B_CYAN;
+    cout<<"\t1. Run Depression Tester\n";
+    cout<<"\t2. View Records\n";
+    cout<<"\t3. Delete Record\n";
+    cout<<"\t4. Disclaimer\n";
+    cout<<"\t5. View Information About this Application\n";
+    cout<<TXT_B_WHITE;
+    cout<<"\nTo exit the application, enter 0.\n\n";
+    cout<<RESET;
+    cin>>choice;
     switch(choice)
     {
         case 0: {
                     void exit(void);
+                    break;
                 }
+
         case 1: {
                     system("clear");
                     write_record();
@@ -38,13 +48,14 @@ int main()
                 }
         
         case 3: {
-                    system("clear");
-                    cout<<"This feature is still in development.\n";
+                    //system("clear");
+                    cout<<"\nThis feature is still in development.\n";
                     break;
                 }
 
         case 4: {
-                    system("clear");
+                    //system("clear");
+                     cout<<"\nThis feature is still in development.\n";
                      /*cout<<"Disclaimer: "<<TXT_B_WHITE<<"The information provided ";
                     cout<<"in this software is intended for general information only and is not legal advice or other professional advice. ";
                     cout<<"Any opinions expressed in this material do not necessarily represent the views of the Copyright Agency.";
@@ -54,10 +65,24 @@ int main()
                     cout<<"damage arising from reliance on material on this website."<<RESET;*/
                     break;
                 }
-        
+
+        case 5: {
+                    //system("clear");
+                     cout<<"This feature is still in development.\n";
+                     /*cout<<"Disclaimer: "<<TXT_B_WHITE<<"The information provided ";
+                    cout<<"in this software is intended for general information only and is not legal advice or other professional advice. ";
+                    cout<<"Any opinions expressed in this material do not necessarily represent the views of the Copyright Agency.";
+                    cout<<"While care and consideration has been taken in the creation of the material on this website, we do not warrant, ";
+                    cout<<"represent or guarantee that the material published on this website is in all respects accurate, complete and current. ";
+                    cout<<"To the extent permitted by law, we exclude any liability, including any liability for negligence, for any loss or ";
+                    cout<<"damage arising from reliance on material on this website."<<RESET;*/
+                    break;
+                }
+
         default: {
                     cout<<"Oops! You entered the wrong input.\n";
-                    cout<<"Restarting the application";
+                    cout<<"Restarting the application in 3 seconds.\n";
+                    sleep(3);
                     main();
 
                  }
